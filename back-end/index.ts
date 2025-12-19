bf60e7d284a85d5a3a3209bd0d2783b1d9fbf01b
David Santos <davidwallacedossantos@gmail.com>
2025-12-19 11:27:48 -0300

    feat: add rota inicial

--- back-end/index.ts
+++ back-end/index.ts
@@ -3,6 +3,10 @@ import express from "express"; //padrão ecma scrypt
 const app = express();
 const PORT = 3000;
 
+app.get("/", (req, res) => {
+  res.json({ msg: "Rota incial" });
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
