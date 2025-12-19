bd133a4dd059d077d5f88890b167b422d992a48f
David Santos <davidwallacedossantos@gmail.com>
2025-12-19 11:15:39 -0300

    feat: criando servidor, instalando nodemon e ts-node

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,1 +1,8 @@
 import express from "express"; //padrão ecma scrypt
+
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
